83f774b47e75603d34c010dbf5d9e8c1b7e55e0a
Umar Mohammed <umar@thinkincode.co.uk>
2021-07-16 14:22:53 +0100

    first commit

---
+++ Week4Api/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Week4Api.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        [HttpGet("api/users")]
+        public string[] GetUsers()
+        {
+            return new string[] { "Umar", "Burhan" };
+        }
+    }
+}
